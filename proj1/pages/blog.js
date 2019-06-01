d37021d3f7aafcf829f46a50f473833bea560e2d
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-06-01 19:57:27 +0530

    posts getting from jsonplaceholder

--- proj1/pages/blog.js
+++ proj1/pages/blog.js
@@ -1,30 +1,45 @@
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
+import { Component } from 'react';
 
 import Layout from '../components/Layout';
 
 
-const PostLink = ({ title }) => (
+const PostLink = ({ title, id }) => (
     <li>
-        <Link href={`/post?title=${title}`}>
+        <Link href={`/post?title=${id}`}>
             <a>Post One About {title}</a>
         </Link>
     </li>
 )
 
+class Blog extends Component {
+
+    static async getInitialProps() {
+        let blogArray = [];
+        for (let i = 1; i < 6; i++) {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${i}`);
+            const statusCode = res.status;
+            const result = await res.json();
+
+            blogArray.push(result);
+        }
+
+        return { posts: blogArray };
+    }
 
 
-const Blog = () => (
-    <Layout>
-        <h3>This is Blog</h3>
-        <ul>
-            <PostLink title="React" />
-            <PostLink title="Vue" />
-            <PostLink title="Angular" />
-        </ul>
-    </Layout>
-)
-
-
+    render() {
+        return (
+            <Layout>
+                <h3>This is Blog</h3>
+                <ul>
+                    {this.props.posts.map(post => <PostLink key={post.id} id={post.id} title={post.title} />)}
+                </ul>
+            </Layout>
+        )
+    }
+    
+}
 
 export default Blog;
