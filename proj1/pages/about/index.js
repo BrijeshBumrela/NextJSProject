f38d8de2ac42c739c3cf5bc5dfa36c924ca72c61
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-06-01 18:46:51 +0530

    modularized css

--- proj1/pages/about/index.js
+++ proj1/pages/about/index.js
@@ -1,26 +1,26 @@
-import Layout from '../components/Layout.js';
+import { Component } from 'react';
 import Head from 'next/head';
 import fetch from 'isomorphic-unfetch';
-import { Component } from 'react';
-import Error from './_error/index';
+
+import Layout from '../../components/Layout.js';
+import Error from '../_error/index';
+
+import moduleCSS from './about.css';
 
 export default class About extends Component {
 
     static async getInitialProps() {
-        const res = await fetch('https://api.github.com/uers/BrijeshBumrela');
+        const res = await fetch('https://api.github.com/users/BrijeshBumrela');
         const statusCode = res.status > 200 ? res.status : false;
         const data = await res.json();
         return { user: data, statusCode }
     }
 
     render() {
-
         const { user, statusCode } = this.props;
-
         if (statusCode) {
             return <Error />
         }
-
         return (
             <React.Fragment>
                 <Head>
@@ -31,6 +31,7 @@ export default class About extends Component {
                     <h2>This is the about page</h2>
                     <h3>My Github Id is {user.login}</h3> 
                     <img 
+                        className={moduleCSS.imageSize}
                         src={user.avatar_url}
                         alt="BrijeshBumrela"
                     />
