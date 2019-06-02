45399e4da9415844ab3fab76d6370a7f19e6744f
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-06-02 23:26:20 +0530

    added cookie with cookie-parser

--- nextCookieAuth/server.js
+++ nextCookieAuth/server.js
@@ -1,5 +1,7 @@
 const next = require('next');
 const express = require('express');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
@@ -7,17 +9,35 @@ const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const AUTH_USER_TYPE = "authenticated";
+const COOKIE_SECRET = "foinvonevo;ndv";
+const COOKIE_OPTIONS = {
+    httpOnly: true,
+    signed: true
+}
+
+const authenticate = async (email, password) => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return data.find(user => user.email === email && user.password === password);
+}
+
 app.prepare().then(() => {
     const server = express();
     server.use(express.json());
+    server.use(cookieParser(COOKIE_SECRET));
 
-    server.post('/api/login', (req, res, next) => {
+    server.post('/api/login',async (req, res, next) => {
         const { email, password } = req.body;
-        res.json({
-            email,
-            password,
-            success: true
-        })
+        const userData = await authenticate(email, password);
+        if (!userData) {
+            return res.status(403).send()
+        }
+        const user = {
+            name: user.name,
+            email: user.email
+        }
+        res.cookie('token', user, COOKIE_OPTIONS);
+        res.json(user);
     })
 
 
