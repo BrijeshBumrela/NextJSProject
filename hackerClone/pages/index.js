719e2d3f8a1c14f328e3d82d93b49a91c24cceb8
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-06-02 09:56:06 +0530

    getting next pages

--- hackerClone/pages/index.js
+++ hackerClone/pages/index.js
@@ -1,32 +1,54 @@
 import fetch from 'isomorphic-unfetch';
 import Error from 'next/error';
+import Link from 'next/link';
 
 import StoryList from '../components/storyList';
 import Layout from '../components/Layout';
 
 class Index extends React.Component {
     
-    static async getInitialProps() {
+    static async getInitialProps({ req, res, query }) {
         let stories = [];
+        let page = Number(query.page) || 1;
+        
         try {
-            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await response.json();    
         } catch(err) {
             console.log(err);
         }
-        return { stories }
+        return { stories, page }
     }
     
     render() {
 
-        const { stories } = this.props;
+        const { stories, page } = this.props;
         if (stories.length === 0) {
             return <Error statusCode="503" />
         }
-
         return (
             <Layout title="HackerNext">
                 <StoryList stories={stories}/>
+                <footer>
+                    <Link href={`/?page=${page + 1}`}>
+                        <a>
+                            Next Page ({page + 1})
+                        </a>
+                    </Link>
+                </footer>
+
+                <style jsx>{`
+                    footer {
+                        padding: 1rem;
+                    }
+                    footer a {
+                        font-weight: bold
+                        color: black;
+                        text-decoration: none;
+                    }
+                `}
+                </style>
+
             </Layout>
         )
     }
